35d6b3bd1fa8ef1f36363d95acf29daaaf9f9481
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-05-25 15:34:21 -0300

    Update PacienteSpec.js

--- src/test/js/PacienteSpec.js
+++ src/test/js/PacienteSpec.js
@@ -6,4 +6,10 @@ describe("Paciente", function(){
         expect(guilherme.imc()).toEqual(72 / (1.82 * 1.82));
     });
 
+    it("Deve informar quantos batimentos o paciente já teve na vida", function(){
+        var guilherme = new Paciente("Guilherme", 28, 72, 1.82);
+
+        expect(guilherme.batimentos()).toEqual(1177344000);
+    });
+
 });
