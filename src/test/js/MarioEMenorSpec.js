da34d556c8848c33d56a0f688037694b21ce9021
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-05-25 12:48:59 -0300

    updates

---
+++ src/test/js/MarioEMenorSpec.js
@@ -0,0 +1,26 @@
+describe("MaiorEmenor", function(){
+    it("Deve entender numeros menor em ordem nao sequencial", function(){
+
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([5,15,7,9]);
+
+        expect(algoritmo.pegaMaior()).toEqual(15);
+        expect(algoritmo.pegaMenor()).toEqual(5);
+    });
+
+    it("Deve entender numeros crescente", function(){
+
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([5,6,7,8]);
+
+        expect(algoritmo.pegaMaior()).toEqual(8);
+    });
+
+    it("Deve entender numeros decrescente", function(){
+
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([5,4,3,2,1]);
+
+        expect(algoritmo.pegaMenor()).toEqual(1);
+    });
+});
