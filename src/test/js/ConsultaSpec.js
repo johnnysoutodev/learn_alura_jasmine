42996025643816ff171953c0e1547bf2a1a144a5
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-05-25 15:14:11 -0300

    updates

---
+++ src/test/js/ConsultaSpec.js
@@ -0,0 +1,23 @@
+describe("Consulta", function(){
+
+    it("Nao deve cobrar nada se for um retorno", function(){
+        var guilherme = new Paciente("Guilherme", 28, 72, 1.82);
+        var consulta = new Consulta(guilherme, [], true, true);
+
+        expect(consulta.preco()).toEqual(0);
+    });
+
+    it("Deve cobrar 25 por cada procediento comum", function(){
+        var johnny = new Paciente("Johnny", 38, 75, 1.76);
+        var consulta = new Consulta(johnny, ["proc1", "proc2"], false, false);
+
+        expect(consulta.preco()).toEqual(50);
+    });
+
+    it("deve cobrar preco especifico dependendo do procedimento", function(){
+        var joao = new Paciente("João", 1, 15, 0.86);
+        var consulta = new Consulta(joao, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false, false);
+
+        expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+    });
+});
